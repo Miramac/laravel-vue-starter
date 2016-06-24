684adb886d3bea25e64a69b1195d33a62e4e631d
Fabian Roloff <fabian.roloff@vocatus.de>
2016-06-24 18:03:27 +0200

    add authenticated state, include footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" id="csrf-token">
+    <meta name="authenticated" content="{{ (Auth::check()) ? 1 : 0 }}">
     <title>Laravel</title>
 
     <!-- Styles -->
@@ -15,8 +16,11 @@
     <div id="app-nav">
         @include('layouts.nav')
     </div>
-    <div>
+    <div id="app-body">
         @yield('content')
+    </div>
+    <div id="app-footer">
+        @include('layouts.footer')
     </div>
     <!-- JavaScripts -->
     <script src="{{ url('/js/app.js') }}"></script>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-default navbar-fixed-bottom nav-bot">
         <div class="container-fluid">
             <div class="footer.html">
-                <p class="navbar-text"><small>&copy; Vocatus AG</small></p>
+                <p class="navbar-text"><small>&copy; Company</small></p>
             </div>
         </div>
     </nav>
